bce60afae9b7a34d343d5055764b2480a3e2b063
jjun-ho <gkswnsgh2@naver.com>
2022-02-13 02:13:55 +0900

    2022.2.13

--- 8. 기본 수학 1/1.py
+++ 8. 기본 수학 1/1.py
@@ -1,2 +1,14 @@
 #1712
 
+A, B, C = map(int,input().split())
+
+if B>=C:
+    print(-1)
+else:
+    print(int(A/(C-B)+1))
+
+
+"""
+A+B*n = C*n
+n= A/(C-B) : 수입과 비용 같아짐 -> 더 클 때:+1
+"""
--- 8. 기본 수학 1/4.py
+++ 8. 기본 수학 1/4.py
@@ -1,1 +1,17 @@
 #2869
+
+import math
+
+A, B, V = map(int,input().split())
+day = math.ceil((V-A)/(A-B)) + 1
+print(day)
+
+"""
+(A-B)*day + A = V
+day = (V-A)/(A-B)
+
+* math
+math 모듈을 이용하면 수학과 관련된 함수들을 사용할 수 있다. 
+ceil 함수를 이용하면 소수를 올림 하는 정수를 반환하고 반대로 소수를 내림한 정수를 반환할 때는 floor를 이용한다.
+ceil은 천장, floor는 바닥을 의미한다고 생각하면 기억하기가 쉽다.
+"""
