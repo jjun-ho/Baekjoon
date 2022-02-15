3e1b03583ae994e2920b142ebd28ff2b43e7cf12
jjun-ho <gkswnsgh2@naver.com>
2022-02-15 18:22:25 +0900

    2022.2.15

--- 9. 기본 수학 2/3.py
+++ 9. 기본 수학 2/3.py
@@ -1,4 +1,13 @@
 #11653
 
-num = int(input())
-
+a = int(input())
+num = []
+i = 1
+while i < a:
+    i += 1
+    if a % i == 0:
+        num.append(i)
+        a = a/i
+        i = 1
+for i in num:
+    print(i)
--- 9. 기본 수학 2/4.py
+++ 9. 기본 수학 2/4.py
@@ -1,1 +1,20 @@
 #1929
+
+M ,N = map(int, input().split())
+num = []
+remove = []
+for i in range(M, N+1):
+    num.append(i)
+for i in range(M,N+1):
+    if i == 1:
+        remove.append(i)
+        continue
+    for j in range(2,int(i**0.5)+1):
+        if i % j == 0:
+            remove.append(i)
+            break
+for i in remove:
+    num.remove(i)
+
+for i in num:
+    print(i)
