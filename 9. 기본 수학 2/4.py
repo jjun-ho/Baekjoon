4f1fb444b073a8d293f7e27884d7aad86b4e0f81
jjun-ho <gkswnsgh2@naver.com>
2022-02-17 01:30:12 +0900

    2022.2.17

--- 9. 기본 수학 2/4.py
+++ 9. 기본 수학 2/4.py
@@ -1,20 +1,27 @@
 #1929
 
-M ,N = map(int, input().split())
-num = []
-remove = []
-for i in range(M, N+1):
-    num.append(i)
-for i in range(M,N+1):
-    if i == 1:
-        remove.append(i)
-        continue
-    for j in range(2,int(i**0.5)+1):
-        if i % j == 0:
-            remove.append(i)
-            break
-for i in remove:
-    num.remove(i)
+def isPrime(num):
+    if num == 1:
+        return False
+    else:
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                return False
+        return True
 
-for i in num:
-    print(i)
+
+M, N = map(int, input().split())
+
+for i in range(M, N + 1):
+    if isPrime(i):
+        print(i)
+
+"""
+소수 판별 알고리즘
+* 에라토스테네스의 체
+에라토스테네스의 체에서는 1을 제거
+--> 지워지지 앟는 수 중에 제일 작은 2를 소수로 선택한다 
+--> 나머지 2의 배수를 모두 지운다 --> 지워지지 않는 수 중에 제일 작은 3을 소수로 선택한다 
+--> 나머지 3의 배수를 지운다 ... 5, 7, 11, 13 등으로 반복을 한다.
+제곱근까지만 약수의 여부를 검증해도 소수인지 아닌지 알 수 있는 이유는 6과 같은 수의 경우 2*3=3*2로 대칭을 이루기 때문이다.
+"""
--- 9. 기본 수학 2/5.py
+++ 9. 기본 수학 2/5.py
@@ -1,2 +1,26 @@
 #4948
 
+def isPrime(n):
+    if n == 1:
+        return False
+    for i in range(2,int(n**0.5)+1):
+        if n%i == 0:
+            return False
+    return True
+
+all_list = list(range(2,246912))		#문제에서 제한한 범위
+memo = []								#제한된 범위 속 소수. for문 밖에 리스트 정의
+
+for i in all_list:						#2부터 2*123,456 범위
+    if isPrime(i):						#isPrime함수에 해당하면
+        memo.append(i)					#리스트에 추가
+
+while True:
+    n = int(input())
+    count = 0					#갯수를 세야하는 조건 때문에 카운트
+    if n == 0 :
+            break
+    for i in memo:			#memo리스트 중에서
+        if n < i <=2*n:		#입력한 값의 범위 내에서 값이 있으면
+            count+=1		#있을 때 마다 카운트 +1
+    print(count)
