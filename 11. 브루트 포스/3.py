ed72c7cb46673e7be3e5ecdc2d1e7a67741f8f41
jjun-ho <gkswnsgh2@naver.com>
2022-03-04 16:46:51 +0900

    2022.3.4

--- 11. 브루트 포스/3.py
+++ 11. 브루트 포스/3.py
@@ -1,1 +1,22 @@
 #7568
+
+n = int(input())
+data = []
+grade = []
+for i in range(n):
+    x, y = map(int,input().split())
+    data.append([x,y])
+for i in range(len(data)):
+    cnt = 0
+    for j in range(0,len(data)):
+        if (data[i][0] < data[j][0]) and (data[i][1] < data[j][1]):
+            cnt += 1
+    grade.append(cnt+1)
+for i in grade:
+    print(i, end=" ")
+
+"""
+*  print() 줄바꿈 제거
+for i in grade:
+    print(i, end=" ")
+"""
