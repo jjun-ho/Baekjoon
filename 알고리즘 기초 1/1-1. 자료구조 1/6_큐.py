a3d230e92ec92097b8b9d05d620647142c6d51ee
jjun-ho <gkswnsgh2@naver.com>
2022-03-13 23:35:36 +0900

    3

--- 알고리즘 기초 1/1-1. 자료구조 1/6_큐.py
+++ 알고리즘 기초 1/1-1. 자료구조 1/6_큐.py
@@ -1,2 +1,32 @@
 #10845
 
+import sys
+n = int(sys.stdin.readline())
+data = []
+for i in range(n):
+    com = list(sys.stdin.readline().split())
+    if com[0] == 'push':
+        data.append(int(com[1]))
+    elif com[0] == 'pop':
+        if len(data) == 0:
+            print(-1)
+        else:
+            print(data[0])
+            data.remove(data[0])
+    elif com[0] == 'front':
+        if len(data) == 0:
+            print(-1)
+        else:
+            print(data[0])
+    elif com[0] == 'back':
+        if len(data) == 0:
+            print(-1)
+        else:
+            print(data[len(data)-1])
+    elif com[0] == 'size':
+        print(len(data))
+    elif com[0] == 'empty':
+        if len(data) == 0:
+            print(1)
+        else:
+            print(0)
