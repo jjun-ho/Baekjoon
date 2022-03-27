6af5d8674cff2d4bccb609f484c7a7db8ccdfde9
jjun-ho <gkswnsgh2@naver.com>
2022-03-27 21:01:54 +0900

    2022.3.27

--- 알고리즘 기초 1/3-1. 다이나믹 프로그래밍 1/5_카드_구매하기.py
+++ 알고리즘 기초 1/3-1. 다이나믹 프로그래밍 1/5_카드_구매하기.py
@@ -1,1 +1,21 @@
 #11052
+
+import sys
+n = int(sys.stdin.readline())
+price = [0]+list(map(int,sys.stdin.readline().split()))
+max = [0] * (n+1)
+max[1] = price[1]
+for i in range(2, n+1):
+    for j in range(1, i+1):
+        if max[i] < max[i-j] + price[j]:
+            max[i] = max[i-j] + price[j]
+print(max[n])
+
+"""
+max[i] = 카드 i개 구매하는 최대 가격 , price[k] = k개가 들어있는 카드팩 가격
+max[1] = price[1]
+max[2] = max[1] + price[1] or max[0] + price[2]
+max[3] = max[2] + price[1] or max[1] + price[2] or max[0] + price[3]
+max[4] = max3] + price[1] or max[2] + price[2] or max1] + price[3] or max[0] + price[4]
+점화식은 max[i] =  max[i - k] + price[k] 
+"""
